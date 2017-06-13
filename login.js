233a3b48b66d50a62a4fef61ec1978febde5eed1
Kulldiep <kulldiep@gmail.com>
2017-06-14 00:22:07 +0530

    Assignment 8.2
    Done

---
+++ login.js
@@ -0,0 +1,18 @@
+function aback() {
+	var aback= 'index.html'
+	
+    window.open(aback , "_self");
+}
+function log() {
+	var log= 'login.html'
+	
+    window.open(log , "_self");
+   
+}
+function enter() {
+	var enter= 'sign-up.html'
+	
+    window.open(enter , "_self");
+   
+}
+
